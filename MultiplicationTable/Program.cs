e9037dfeb628b46daca75871e03e589470f6bf5f
Tiraniell <93056140+Tiraniell@users.noreply.github.com>
2023-01-28 12:31:31 +0300

    Добавьте файлы проекта.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (i > 0 && j > 0)
+                    {
+                        int result = j * i;
+
+                        Console.Write($"{result, -4}" ); // - {переменная, спецификатор форматирования}
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
